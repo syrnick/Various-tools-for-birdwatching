57536e570f935c208749c862d7160595c6844ab1
Alex Sorokin <alex@crowdflower.com>
2011-07-17 20:50:07 -0700

    Switched google map parsing to KML format

--- google_map_to_landmarks.rb
+++ google_map_to_landmarks.rb
@@ -3,25 +3,29 @@ require 'nokogiri'
 require 'open-uri'
 require 'yaml'
 
-default_map_url='http://maps.google.com/maps/ms?ie=UTF8&hl=en&vps=1&jsv=304e&msa=0&output=georss&msid=202933467189883314896.00049912d6823abb690f2'
+default_map_url='http://maps.google.com/maps/ms?authuser=0&vps=3&hl=en&ie=UTF8&msa=0&output=kml&msid=202933467189883314896.00049a04d9feb0cc487f6'
 
 map_url = ARGV[0] || default_map_url
 
 doc = Nokogiri::XML( open(map_url) )
 
+#p doc.xpath('/kml')
+
 paths = []
-doc.xpath('//item[gml:LineString]').each do |path|
-  landmark_name = path.xpath("title").first.text
-  path = path.xpath("gml:LineString/gml:posList").first.text
-  long_lat_path = path.strip.split("\n").map {|l| l.strip.split(' ').map {|v| v.to_f}}
+doc.xpath('/kml/Document/Placemark').each do |path|
+  landmark_name = path.xpath("name").first.text
+  path = path.xpath(".//coordinates").first.text
+  long_lat_path = path.strip.split("\n").map {|l| a = l.strip.split(',').map {|v| v.to_f}; [a[1],a[0]]}
   paths << {:name => landmark_name, :path =>long_lat_path, :type => 'path'}
 end
 
-doc.xpath('//item[gml:Polygon]').each do |path|
-  landmark_name = path.xpath("title").first.text
-  path = path.xpath("gml:Polygon/gml:exterior/gml:LinearRing/gml:posList").first.text
-  long_lat_path = path.strip.split("\n").map {|l| l.strip.split(' ').map {|v| v.to_f}}
-  paths << {:name => landmark_name, :path =>long_lat_path, :type => 'path'}
-end
+#p paths
+
+# doc.xpath('//item[gml:Polygon]').each do |path|
+#   landmark_name = path.xpath("title").first.text
+#   path = path.xpath("gml:Polygon/gml:exterior/gml:LinearRing/gml:posList").first.text
+#   long_lat_path = path.strip.split("\n").map {|l| l.strip.split(' ').map {|v| v.to_f}}
+#   paths << {:name => landmark_name, :path =>long_lat_path, :type => 'path'}
+# end
 
 puts YAML.dump(paths)
